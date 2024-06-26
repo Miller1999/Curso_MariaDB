dba9f4940e8dceadccea819a9d716e856bcaa4f7
Miller Arias <millerjavierarias@gmail.com>
2024-06-25 22:14:59 -0500

    active drivers, drivers and trigger

--- 18-triggers.sql
+++ 18-triggers.sql
@@ -1,5 +1,6 @@
 USE metro_cdmx;
 
+-- De esta manera se crean los triggers con el valor NEW actualizamos el campo con la nueva informacion, 
 CREATE TRIGGER update_updated_at_field BEFORE
 UPDATE ON lineas FOR EACH ROW
 SET
---
+++ 21-fill-drivers-table.sql
@@ -0,0 +1,10 @@
+USE metro_cdmx;
+
+INSERT INTO
+  drivers (name)
+VALUES
+  ("Pedro"),
+  ("Pablo"),
+  ("Jose"),
+  ("Maria"),
+  ("Amanda");
